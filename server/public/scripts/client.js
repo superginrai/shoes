a0e52718d28d929e150dc88cc3373ed777fbf0c6
Cory Booth <Rodimus2005@hotmail.com>
2018-05-03 14:37:10 -0500

    get post update delete working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,16 +1,23 @@
+console.log('JSSSSS');
+
+let app = angular.module('ShoeApp', []);
+
 app.controller('ShoeController', ['$http', function ($http) {
     console.log('ShoeController has been loaded');
     let self = this;
+    //self.shoes = [];
 
+    self.shoes = { data: '' };
 
     self.listShoes = function () {
+        console.log('you clicked for shoes!!!')
         $http({
             method: 'GET',
-            url: '/shoes'
+            url: '/shoe'
         })
             .then(function (response) {
                 console.log(response);
-                self.things = response.data;
+                self.shoes.data = response.data;
             })
             .catch(function (error) {
                 console.log('error on /shoes GET', error);
@@ -21,15 +28,48 @@ app.controller('ShoeController', ['$http', function ($http) {
         console.log('Add this shoe:', self.newShoe);
         $http({
             method: 'POST',
-            url: '/shoes',
+            url: '/shoe',
             data: self.newShoe
         })
             .then(function (response) {
                 console.log('hoorah you got a shoe!', response);
-                self.listThings();
+                self.listShoes();
             })
             .catch(function (error) {
                 console.log('error on /shoes POST', error);
             });
     }
+
+    self.deleteShoe = function (yurShoe) {
+        console.log(yurShoe, 'iz baleted')
+        $http({
+            method: 'DELETE',
+            url: '/shoe',
+            params: yurShoe
+        })
+            .then(function (response) {
+                console.log(response);
+                self.listShoes();
+            })
+            .catch(function (error) {
+                console.log('error on shoe DELETE', error);
+            });
+    }
+
+    self.updateShoe = function (yurShoe) {
+        console.log(yurShoe, 'will get patches')
+        $http({
+            method: 'PUT',
+            url: '/shoe',
+            data: yurShoe
+        })
+            .then(function (response) {
+                console.log(response);
+                self.listShoes();
+            })
+            .catch(function (error) {
+                console.log('error on /shoe PUT', error);
+            });
+    }
+
 }])
--- server/server.js
+++ server/server.js
@@ -1,7 +1,26 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const pg = require('pg');
 const PORT = process.env.PORT || 5000;
+
+const Pool = pg.Pool;
+
+const pool = new Pool({
+    database: 'shoe_store',
+    host: 'localhost',
+    port: 5432,
+    max: 10,
+    idleTimeoutMillis: 30000
+});
+
+pool.on('connect', () => {
+    console.log('Postgresql connected');
+});
+
+pool.on('error', (error) => {
+    console.log('Error with postgres pool', error)
+});
 
 app.use(bodyParser.json());
 
@@ -11,16 +30,56 @@ const shoes = [{ name: 'Red Wing', cost: 250 }, { name: 'Puma Soliel v2', cost: 
 
 app.get('/shoe', (req, res) => {
     console.log('GET: /shoe)');
-    res.send(shoes);
-
+    //const allShoes = res.data
+    pool.query(`SELECT * FROM "shoes";`)
+        .then((results) => {
+            res.send(results.rows);
+        })
+        .catch((error) => {
+            console.log('SQL GET error', error);
+            res.sendStatus(500);
+        });
 });
 
 app.post('/shoe', (req, res) => {
-    console.log('POST: new Shoe');
-    const newShoe = req.body
-    shoes.push(newShoe)
-    res.send(shoes)
+    const shoe = req.body;
+    pool.query(`INSERT INTO "shoes" ("name", "cost")
+    VALUES ($1, $2);`, [shoe.name, shoe.cost])
+        .then((results) => {
+            res.sendStatus(200);
+        })
+        .catch((error) => {
+            console.log('SQL POST error:', error);
+            res.sendStatus(500);
+        });
 });
+
+app.delete('/shoe', (req, res) => {
+    console.log(req.query.id);
+    const shoe = req.query.id;
+    pool.query(`DELETE FROM "shoes"
+    WHERE "id" = ${shoe};`)
+    .then(() => {
+        res.sendStatus(200);
+    })
+    .catch((error) => {
+        console.log('DELETE shoe error', error);
+        res.sendStatus(500);
+    })
+})
+
+app.put('/shoe', (req, res) => {
+    const shoe = req.body
+    pool.query(`UPDATE "shoes" SET "name" = ($1), "cost" = ($2) WHERE "id" = ($3)`, [shoe.name, shoe.cost, shoe.id])
+    .then(() => {
+        res.sendStatus(200);
+    })
+    .catch((error) => {
+        console.log('DELETE shoe error', error);
+        res.sendStatus(500);
+    })
+})
+
 
 app.listen(PORT, () => {
     console.log('listenting on port', PORT);
